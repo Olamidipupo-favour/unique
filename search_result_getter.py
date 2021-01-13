1f567e4f949aba36b44011aa568a327105ea7f4f
Rayyan Nafees <59685062+RayyanNafees@users.noreply.github.com>
2021-01-14 01:35:46 +0530

    app.py
    Changed the syntax to make it explicit and readable

--- search_result_getter.py
+++ search_result_getter.py
@@ -1,8 +1,8 @@
 from math import floor
 import inito
-def getSearchResult():
-    return [i.val() for i in db.child(child).get().each()]
-def mwatch(d:str,d1:str,d2:str):
+
+getSearchResult = lambda: [i.val() for i in db.child(child).get().each()]
+def mwatch(d:str,d1:str,d2:str) -> bool:
     d=d.strip()
     word_count=0
     for i in d.split():
@@ -10,7 +10,7 @@ def mwatch(d:str,d1:str,d2:str):
             word_count+=1
     if(word_count>0 and word_count > floor(len(d.split())/2)):
         return True
-def tfidf(d):
+def _tfidf(d):
     #g=d
     #d=[i[2] for i in g]
     #t={}
@@ -24,31 +24,38 @@ def tfidf(d):
     #print(str(t))
     return d
     #will return a tfidf later in future.
-def perfect_result(search):
-    if(not search or search==None):
-        return []
-    else:
-        res=[]
-        #get results.
-        for i in getSearchResult():
-            if(mwatch(search,i[0],i[1])):
-                res.append(i)
-        return tfidf(res)
+
+tfidf = lambda d:d
+perfect_result = lambda search: [] if not search else tfidf([i for i in getSearchResult()
+
 from flask import Flask, request,redirect, url_for, abort, render_template
+
 app=Flask(__name__)
+
 @app.route("/")
 def rindex():
-    return redirect(url_for("index"))
+    return redirect("/index")
+
 @app.route("/index",methods=['GET','POST'])
 def index():
-    if(request.method=="POST"):
-        return render_template('index.html',res=perfect_result(request.form.get('q',None)),q=request.form.get('q',None))
+    if request.method=="POST":
+        _q = request.form.get('q')
+        return render_template('index.html',
+                             res=perfect_result(_q),
+                             q=_q, )
     else:
         return render_template('index.html')
+
+
 @app.errorhandler(404)
 def err_404(e):
-   return redirect(url_for("index"))
-if __name__=='__main__':
-    app.run("0.0.0.0")
+   return redirect("/index")
+
+
+if __name__=='__main__': 
+    app.run(debug=True, port=9999)
+    
+    import webbrowser
+    webbrowser.open("http://localhost:9999/")
 
 
